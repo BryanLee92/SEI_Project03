4b775656b7056aaebdbc714ffcdde99b702a63d0
BryanLee92 <lwm.bryan92@gmail.com>
2022-11-22 01:35:07 +0800

    render.com

--- music_app/src/components/useAuth.js
+++ music_app/src/components/useAuth.js
@@ -8,7 +8,7 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post("http://localhost:3030/login", {
+      .post("https://musicapi.onrender.com/login", {
         code,
       })
       .then(res => {
@@ -26,7 +26,7 @@ export default function useAuth(code) {
     if (!refreshToken || !expiresIn) return
     const interval = setInterval(() => {
       axios
-        .post("http://localhost:3030/refresh", {
+        .post("https://musicapi.onrender.com/refresh", {
           refreshToken,
         })
         .then(res => {
